4f9e4dbb48600b3a9fb00da39b736bfe10fb1156
João Bosco <joao.lima@gertec.com.br>
2023-01-09 17:36:12 -0300

    Shared View Model

--- shared/src/commonMain/kotlin/br/com/aplicativo/appkmm/Platform.kt
+++ shared/src/commonMain/kotlin/br/com/aplicativo/appkmm/Platform.kt
@@ -1,7 +1,14 @@
 package br.com.aplicativo.appkmm
 
+import kotlinx.coroutines.CoroutineScope
+
 interface Platform {
     val name: String
 }
 
 expect fun getPlatform(): Platform
+
+expect abstract class BaseSharedViewModel() {
+    val scopo: CoroutineScope
+    protected fun onCleared()
+}
---
+++ shared/src/commonMain/kotlin/br/com/aplicativo/appkmm/viewmodels/SampleViewModel.kt
@@ -0,0 +1,9 @@
+package br.com.aplicativo.appkmm.viewmodels
+
+import br.com.aplicativo.appkmm.BaseSharedViewModel
+
+/**
+ * Created by João Bosco on 09/01/2023.
+ */
+class SampleViewModel : BaseSharedViewModel() {
+}
